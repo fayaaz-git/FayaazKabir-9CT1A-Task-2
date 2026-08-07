12ccb885940d8934c55f4f9abb477db722ba4d37
fayaaz-git <fayaaz.kabir841@education.nsw.gov.au>
2026-08-07 12:05:32 +1000

    Tried to make the sensor cooperate.
    I haven't been able to extract the values the sensor records whatsoever, and I'm pretty sure it's the wiring. I have turned all of the code I've added into a docstring so that it doesn't interfere with existing code.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# import stuff here
+# everything required is imported here
 
-from machine import Pin
+from machine import Pin # this is where you control inputs and outputs in a 
 import time
 
 # setup here 
@@ -35,5 +35,18 @@ def potential_flame(y, b):
 
 # main routine here
 
-led = Pin(15, Pin.OUT)	# Sets up pin 15 to power in order to turn on the blue LED
-led.value(1)			# Turn on the pin
+led = Pin(15, Pin.OUT);	# Sets up pin 15 to power in order to turn on the blue LED
+led.value(1)
+
+"""Gonna try to test the flame sensor here.
+Hopefully it works, I need to see what values they hold"""
+
+"""sensor_pin = ADC(Pin(26));				""""""I think this pin is the one receiving the sensor values?
+                                        It is the one connected to GND (Ground, I assume).
+                                        The sensor is glowing green and red so hopefully it works
+                                        Anyway I took this from the PWM tutorial, which uses a potentiometer.""""""
+
+while True:
+    sensor_value = sensor_pin.read_u15()	# Supposed to read the value the sensor receives
+    print(sensor_value)
+    time.sleep(0.5)
